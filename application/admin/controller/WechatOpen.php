2ec184f37c507c330dc54f302f861406f4555d4c
iszmxw <mail@54zm.com>
2019-12-19 15:42:21 +0800

    先下拉=>后上传

--- application/admin/controller/WechatOpen.php
+++ application/admin/controller/WechatOpen.php
@@ -8,6 +8,7 @@ namespace app\admin\controller;
 use app\common\logic\AdminLogic;
 use app\common\logic\ModuleLogic;
 use think\Page;
+use think\Request;
 use think\Verify;
 use think\Loader;
 use think\Db;
@@ -22,5 +23,16 @@ class WechatOpen extends Base
     public function auth()
     {
         dump(1);
+    }
+
+    /**
+     * 消息与事件接收URL
+     * @author: iszmxw <mail@54zm.com>
+     * @Date：2019/12/19 15:36
+     */
+    public function message_callback(Request $request)
+    {
+        $appid = $request->input('appid');
+        dump($appid);
     }
 }
