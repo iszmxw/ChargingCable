be4f106f00e659883c26362040ecfc06301e9dbe
iszmxw <mail@54zm.com>
2019-12-19 17:28:53 +0800

    先下拉=>后上传

--- application/admin/controller/WechatOpen.php
+++ application/admin/controller/WechatOpen.php
@@ -36,7 +36,7 @@ class WechatOpen extends Base
     {
         $user_id = $request->get('user_id');
 
-        $config = $this->openPlatform->getAuthorizerOption();
+        $config = $this->openPlatform->getAuthorizerInfo();
         dump($config);
         die();
         $appid         = $config['app_id'];
