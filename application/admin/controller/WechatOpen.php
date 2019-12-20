5416754e1d18775ce62782a355dccf36addaa67e
iszmxw <mail@54zm.com>
2019-12-20 15:23:55 +0800

    先下拉=>后上传

--- application/admin/controller/WechatOpen.php
+++ application/admin/controller/WechatOpen.php
@@ -21,7 +21,6 @@ class WechatOpen extends Base
         $redis       = new \Redis();
         $redis->connect('118.89.61.124', 4399);
         $redis->auth('blog_54zm_com');              //密码验证
-        $redis->select(2);                          //选择数据库2
         $cacheDriver->setRedis($redis);
         $options            = [
             'debug'   => true,
