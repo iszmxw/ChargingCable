37f3e5b16beba6a9eab95a2220b16de5199d0a0e
iszmxw <mail@54zm.com>
2019-12-20 10:12:20 +0800

    先下拉=>后上传

--- application/config.php
+++ application/config.php
@@ -467,5 +467,11 @@ return [
         "AppSecret" => "b98a2b814fb7e3a548d42367c81abd2e",
         "Token"     => "iszmxw",
         "Aes_Key"   => "ckGPqhPfREgJZR6rC8rz3xqQcdmZRf8Xv9QMm5ym3Yf"
+    ],
+    "WechatOpen2"             => [// 微信开放平台参数
+        "AppId"     => "wx6590d39e4f1bf4a0",
+        "AppSecret" => "d290f710854a122f7eebc11bb8bc2ec2",
+        "Token"     => "iszmxw",
+        "Aes_Key"   => "ckGPqhPfREgJZR6rC8rz3xqQcdmZRf8Xv9QMm5ym3Yf"
     ]
 ];
